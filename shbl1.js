8dd515321d9d79ef90d170b6ad1f07ede6149e00
Shbl777217 <115428778+Shbl777217@users.noreply.github.com>
2022-12-24 02:53:48 +0300

    Update shbl1.js

--- shbl1.js
+++ shbl1.js
@@ -2954,53 +2954,5 @@ background-size: 220% auto;border-radius: 50px !important;background-color: #000
 
 
 
-$("style").last().append(`
-#users .uzr.stl1 .u-msg {
-  -webkit-background-clip: text;
-  padding: 1px !important;
-  font-size: 89% !important;
-  -webkit-text-fill-color: #0000;
- background-image: url(https://up6.cc/2022/12/167183568860261.gif);
-  padding: 0px !important;
-  font-size: 89% !important;
-	margin-bottom: 6px !important;
-background-size: cover;
-}
-#users .uzr.stl1 {
-  box-shadow: inset 0 0 0 rgba(0,0,0,.08),0 0 2px #000;
-  margin-bottom: 3px !important;
-  margin-top: 2px !important;
-  border-radius: 4px;
-  background-image: 
-  background-size: cover;
-  border: 1px solid #fff;
-background-image: linear-gradient(-225deg, rgb(202, 202, 202) 0%, rgba(255, 0, 143, 0) 29%, rgba(151, 101, 127, 0.04) 67%, rgb(198, 198, 198) 100%);  
-}
-#users .uzr.stl1 .d-flex.fl {
-  padding-right: 0px !important;
-}
-#users .uzr.stl1 .u-topic {
-  margin-left: 5px;
-}
-`);
-var l_itv=setInterval(function(){
-    if(myid!=null){
-        clearInterval(l_itv);
-        $("#users .uzr:contains('                                                              ‏𝐌𝐬𝐡 ')").addClass('stl1');
-        $("div#users .uzr:contains('                                                              ‏𝐌𝐬𝐡 ') ").click(function(){
-        $("#movon").show();
-        
-        $('#movon').addClass('stl1');
-      
-        
-        
-    
-       $("#ifr_mov").show(function(){
-        $("#upro").modal("hide");
-    } ); 
-});
-    }
-}, 20000);
-
 
   
