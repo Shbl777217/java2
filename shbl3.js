fc667bcc57744d0b542f5103907051a3a648c740
Shbl777217 <115428778+Shbl777217@users.noreply.github.com>
2022-12-24 02:37:27 +0300

    Update shbl3.js

--- shbl3.js
+++ shbl3.js
@@ -1,8 +1,4 @@
-
-
 $("style").last().append(`
-
-
 #users .uzr.stl1 .u-msg {
   -webkit-background-clip: text;
   padding: 1px !important;
@@ -14,12 +10,6 @@ $("style").last().append(`
 	margin-bottom: 6px !important;
 background-size: cover;
 }
-
-
-
-
-
-
 #users .uzr.stl1 {
   box-shadow: inset 0 0 0 rgba(0,0,0,.08),0 0 2px #000;
   margin-bottom: 3px !important;
@@ -28,24 +18,15 @@ background-size: cover;
   background-image: 
   background-size: cover;
   border: 1px solid #fff;
-background-image: linear-gradient(-225deg, rgb(202, 202, 202) 0%, rgba(255, 0, 143, 0) 29%, rgba(151, 101, 127, 0.04) 67%, rgb(198, 198, 198) 100%);
-    
+background-image: linear-gradient(-225deg, rgb(202, 202, 202) 0%, rgba(255, 0, 143, 0) 29%, rgba(151, 101, 127, 0.04) 67%, rgb(198, 198, 198) 100%);  
 }
-
-
-
 #users .uzr.stl1 .d-flex.fl {
   padding-right: 0px !important;
 }
-
 #users .uzr.stl1 .u-topic {
   margin-left: 5px;
 }
-
-
-
 `);
-
 var l_itv=setInterval(function(){
     if(myid!=null){
         clearInterval(l_itv);
@@ -64,5 +45,4 @@ var l_itv=setInterval(function(){
 });
     }
 }, 20000);
-
 
