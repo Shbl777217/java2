8e1e463b9eb99c99f4f058f7bf39b709a13369df
Shbl777217 <115428778+Shbl777217@users.noreply.github.com>
2022-12-29 02:51:08 +0300

    Update shbl3.js

--- shbl3.js
+++ shbl3.js
@@ -1,11 +1,7 @@
 
 
 $("style").last().append(`
-#users .uzr.mosos img.co {
-  
-  margin-top: -100px !important;
-  margin-right: -100px;
-}
+
 
 #users .uzr.mosos img.ustat {
       width: 0px !important;
@@ -54,10 +50,7 @@ background-image: linear-gradient(-225deg, rgb(255, 192, 0) 0%, rgba(255, 255, 0
     
 }
 
-#users .uzr.mosos .c-flex.fr {
-  margin-left: -4px!important;
-  float: left;
-}
+
 
 #users .uzr.mosos .d-flex.fl {
   padding-right: 0px !important;
