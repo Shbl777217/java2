d14652c2a37adfbcbddeac7cc20c0536c22fba56
Shbl777217 <115428778+Shbl777217@users.noreply.github.com>
2023-01-15 02:56:23 +0300

    Update shbl3.js

--- shbl3.js
+++ shbl3.js
@@ -1382,3 +1382,107 @@ var l_itvwaed=setInterval(function(){
     }
 }, 20000);
 
+
+
+
+
+
+
+
+
+$("style").last().append(`
+
+
+
+
+
+
+
+#users .uzr.sig8 img.ustat {
+      width: 54px !important;
+  border-radius: 0px 50px 50px 50px !important;
+  height: 52px !important;
+  margin-top: 5px !important;
+  min-height: 0% !important;
+  margin-left: 10px;
+  z-index: 0 !important;
+  display: none;
+      
+    }
+
+#users .uzr.sig8 .fitimg.u-pic {
+	padding: 1px !important;
+  border-radius: 100px !important;
+  margin-top: 5px !important;
+  height: 52px !important;
+  margin-left: 12px!important;
+  z-index: 0 !important;
+margin-bottom: 9px !important;
+border:1px solid #fff;margin-top:2px;border-radius:5px;
+box-shadow:inset 0 0 0 rgba(0,0,0,.08),0 0 5px #ffc000
+}
+
+#users .uzr.sig8 .u-msg {
+	text-align: center;
+  -webkit-background-clip: text;
+  padding: 1px !important;
+  font-size: 89% !important;
+  -webkit-text-fill-color: #0000;
+ background-image: url(https://up6.cc/2023/01/167260977253491.gif);
+  padding: 0px !important;
+  font-size: 89% !important;
+	margin-bottom: 6px !important;
+background-size: cover;
+
+}
+
+
+
+
+
+#users .uzr.sig8 {
+  box-shadow: inset 0 0 0 rgba(0,0,0,.08),0 0 2px #000;
+  margin-bottom: 3px !important;
+  margin-top: 2px !important;
+  border-radius: 4px;
+  background-image: 
+  background-size: cover;
+  border: 1px solid #fff;
+background-image: linear-gradient(-200deg, rgb(255, 192, 0) 0%, rgba(255, 255, 0, 0.01) 10%, rgba(255, 255, 0, 0.01) 89%, rgb(255, 192, 0) 100%);
+    
+}
+
+
+
+#users .uzr.sig8 .d-flex.fl {
+  padding-right: 0px !important;
+}
+
+#users .uzr.sig8 .u-topic {
+  margin-left: 5px;
+}
+
+#users .uzr.sig8 .My_itar49 {
+ width: 53px;
+height: 37px;
+margin-top: 23px;
+cursor: pointer;
+margin-left: -3px;
+}
+
+
+
+
+`);
+
+var l_itvsig=setInterval(function(){
+    if(myid!=null){
+        clearInterval(l_itvsig);
+       $("#users .uzr:contains('94')").each(function(i,e){ e=$(e); if(e.find('.u-topic').text()=='*S+igma'){e.addClass('sig8')}})
+        $("#users .uzr.sig8 .fitimg.u-pic").append('<img class="My_itar49" title="اسد" src="https://up6.cc/2023/01/167252637762911.gif">');
+     
+
+    }
+}, 20000);
+
+
