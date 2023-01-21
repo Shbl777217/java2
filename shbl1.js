269262c044433f7e9ca3e81919962807a75733dd
Shbl777217 <115428778+Shbl777217@users.noreply.github.com>
2023-01-21 23:27:31 +0300

    Update shbl1.js

--- shbl1.js
+++ shbl1.js
@@ -917,12 +917,8 @@ $(`<style>.nosel.d-flex.room.r17fc704ec53x2aaba417-x-923hp0 .label-primary {back
     $("style").last().append(`
     
     #users.h91 .inroom.uzr img.ustat {
-      width: 54px !important;
-      border-radius: 10px 0px 0px 10px;
-      height: 52px;
-      margin-top: -1px;
-      min-height: 0% !important;
-    }
+      width:10px!important;height:10px!important;min-height:0%!important;margin-right:2px;z-index:999999;border-radius:4px 0px 0px 4px;border:1px solid #fff0}
+    
     #users.h91 .inroom.uzr .fitimg.u-pic {
       border-radius: 10px 0px 0px 10px;
     margin-top: 0px;
