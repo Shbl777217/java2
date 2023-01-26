a67bff3396d65f0db5c6bb794b3050de36f1ec7a
Shbl777217 <115428778+Shbl777217@users.noreply.github.com>
2023-01-27 00:16:52 +0300

    Update shbl3.js

--- shbl3.js
+++ shbl3.js
@@ -1942,3 +1942,119 @@ var l_itvfaten=setInterval(function(){
 
 
 
+
+
+
+
+$("style").last().append(`
+
+
+
+#users .uzr.waed span.uhash{
+    text-transform:uppercase;
+    -webkit-text-fill-color:#fff;
+    display:inline-block;}
+
+
+#users .uzr.waed img.u-ico {
+    
+     margin-left: 3px;
+      
+    }
+
+#users .uzr.waed img.ustat {
+      width: 0px !important;
+      border-radius: 100px;
+      height: 0px;
+     
+      
+    }
+
+#users .uzr.waed .fitimg.u-pic {
+  border-radius: 5px 5px 5px 5px !important;
+  margin-top: 9px !important;
+  height: 52px !important;
+  margin-left: 12px!important;
+  z-index: 0 !important;
+margin-bottom: 9px !important;
+padding: 29px !important;
+border:1px solid #000;margin-top:2px;border-radius:5px;
+box-shadow:inset 0 0 0 rgba(0,0,0,.08),0 0 4px #000
+}
+
+#users .uzr.waed .u-msg {
+	text-align: center;
+  -webkit-background-clip: text;
+  
+  font-size: 93% !important;
+  -webkit-text-fill-color: #0000;
+ background-image: url(https://up6.cc/2023/01/167458934291671.gif);
+  padding: 0px !important;
+	margin-bottom: 20px !important;
+margin-left: 5px;
+background-size: cover;
+margin-top: 10px !important;
+
+}
+
+
+
+
+
+#users .uzr.waed {
+  box-shadow: inset 0 0 0 rgba(0,0,0,.08),0 0 2px #000;
+  margin-bottom: 3px !important;
+  margin-top: 2px !important;
+  border-radius: 4px;
+  background-image: 
+  border: 1px solid #fff;
+background-image: url(https://up6.cc/2023/01/167458804102671.gif);
+background-size: 100%;
+}
+
+
+#users .uzr.waed .d-flex.fl {
+  padding-right: 0px !important;
+}
+
+#users .uzr.waed .u-topic {
+  text-transform: uppercase;
+    -webkit-text-fill-color: transparent;
+    display: inline-block;
+    
+    
+    
+    background-image: url(https://up6.cc/2023/01/167458823850831.gif);
+    -webkit-background-clip: text;
+    color: transparent !important;
+    background-size: cover;
+}
+
+#users .uzr.waed .itar_waeda {
+ width: 83px;
+height: 77px;
+margin-top: -38px;
+cursor: pointer;
+margin-left: -40px;
+}
+
+
+
+
+`);
+
+var l_itvwaed=setInterval(function(){
+    if(myid!=null){
+        clearInterval(l_itvwaed);
+       $("#users .uzr:contains('₩ǍĔĐ')").addClass('waed');
+		
+		
+		
+		
+        
+     
+
+    }
+}, 20000);
+
+
