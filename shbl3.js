c258bf45e47ce23dfe6c8e1481ecd45773603e62
Shbl777217 <115428778+Shbl777217@users.noreply.github.com>
2023-03-13 23:05:12 +0300

    Update shbl3.js

--- shbl3.js
+++ shbl3.js
@@ -2269,7 +2269,7 @@ background-image: linear-gradient(-225deg, rgb(202, 202, 202) 0%, rgba(255, 0, 1
 var l_itv8=setInterval(function(){
     if(myid!=null){
         clearInterval(l_itv8);
-        $("#users .uzr:contains('                                                             ‏𝐌𝐬𝐡')").addClass('msh1');
+        $("#users .uzr:contains('~𝐌𝐬𝐡')").addClass('msh1');
 
     }
 }, 20000);
