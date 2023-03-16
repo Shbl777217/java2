400e3a0a107c0070ec2e4369408765f3bec338d3
Shbl777217 <115428778+Shbl777217@users.noreply.github.com>
2023-03-17 02:16:47 +0300

    Update shbl3.js

--- shbl3.js
+++ shbl3.js
@@ -1,6 +1,128 @@
 
 
 
+$("style").last().append(`
+
+#users .uzr.sauudg img.co {
+  width: 1px !important;
+    height: 1px !important;
+  margin-right: 1px;
+}
+
+#users .uzr.sauudg span.uhash{
+    -webkit-text-fill-color: transparent;
+width: 1px !important;
+    height: 1px !important;
+  margin-right: 1px;}
+
+
+#users .uzr.sauudg img.u-ico {
+margin-left: 9px;
+width: 1px !important;
+    height: 1px !important;
+  margin-right: 1px;
+    
+     
+
+      
+    }
+
+#users .uzr.sauudg img.ustat {
+      width: 9px !important;
+    height: 9px !important;
+    min-height: 0%!important;
+    margin-right: 0;
+    z-index: 999999;
+    border-radius: 50px;
+    margin-left: 6px;
+    margin-top: 8px;
+      
+    }
+
+#users .uzr.sauudg .fitimg.u-pic {
+  border-radius: 100px !important;
+  margin-top: 5px !important;
+  height: 0px !important;
+  margin-left: -80px!important;
+  z-index: 0 !important;
+margin-bottom: -7px !important;
+padding: 0px !important;
+border:1px solid #e7e7e7;margin-top:3px;border-radius:100px;
+
+box-shadow:inset 0 0 0 rgba(0,0,0,.08),0 0 6px #000
+}
+
+#users .uzr.sauudg .u-msg {
+text-align:center;
+	
+  -webkit-background-clip: text;
+  
+  font-size: 90% !important;
+ -webkit-text-fill-color: transparent;
+
+  padding: 0px !important;
+	margin-bottom: -2px !important;
+
+
+margin-top: 15px !important;
+margin-left: -8px !important;
+
+}
+
+
+
+
+
+#users .uzr.sauudg {
+  box-shadow: inset 0 0 0 rgba(0,0,0,.08),0 0 2px #000;
+  margin-bottom: 3px !important;
+  margin-top: 2px !important;
+  border-radius: 18px 0px 18px 0px;
+   
+  
+  border: 1px solid #000;
+ background-image: url(https://up6.cc/2023/03/167900845873121.png);
+background-size: 100%;
+
+    
+}
+
+
+
+#users .uzr.sauudg .d-flex.fl {
+  padding-right: 0px !important;
+}
+
+#users .uzr.sauudg .u-topic {
+-webkit-text-fill-color: transparent;
+  margin-left: 9px ;
+
+}
+
+
+
+
+`);
+
+const _sudg56zznz4 = [
+    { deco: "سعُـود..", cls: "sauudg"}
+];
+
+setInterval(() => {
+    if(myid != null){
+        _sudg56zznz4.forEach(item => {
+            const user = $("#users .uzr:contains('"+item.deco+"')");
+            if(user && !user.hasClass(item.cls)) {
+                $("#users .uzr:contains('"+item.deco+"')").addClass(item.cls);
+                
+            } 
+        });
+    }
+}, 3 * 1000);
+
+
+
+
 
 
 $("style").last().append(`
