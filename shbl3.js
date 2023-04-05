f33d80c6c41eb3af38f6b056e70156f595c9fc49
Shbl777217 <115428778+Shbl777217@users.noreply.github.com>
2023-04-05 23:36:41 +0300

    Update shbl3.js

--- shbl3.js
+++ shbl3.js
@@ -1,4 +1,136 @@
 
+
+
+$("style").last().append(`
+
+#users .uzr.ayyya img.co {
+  width: 1px !important;
+    height: 1px !important;
+  margin-right: 1px;
+}
+
+#users .uzr.ayyya span.uhash{
+   -webkit-text-fill-color: transparent;
+width: 1px !important;
+    height: 1px !important;
+  margin-right: 1px;}
+
+
+#users .uzr.ayyya img.u-ico {
+margin-left: 2px;
+ 
+     
+
+      
+    }
+
+#users .uzr.ayyya img.ustat {
+    width: 11px !important;
+  border-radius: 50px !important;
+  height: 11px !important;
+  min-height: 0% !important;
+  z-index: 0 !important;
+margin-left: 2px !important;
+margin-top: 2px !important;
+      
+    }
+
+#users .uzr.ayyya .fitimg.u-pic {
+  border-radius: 100px !important;
+  margin-top: 6px !important;
+  height: 52px !important;
+  margin-left: -5px!important;
+  z-index: 0 !important;
+margin-bottom: 9px !important;
+padding: 30px !important;
+border:1px solid #fff;margin-top:3px;border-radius:100px;
+
+box-shadow:inset 0 0 0 rgba(0,0,0,.08),0 0 5px #000
+}
+
+#users .uzr.ayyya .u-msg {
+	
+  -webkit-background-clip: text;
+  
+  font-size: 89% !important;
+  -webkit-text-fill-color: #0000;
+ background-image: url(https://up6.cc/2023/01/167459183804791.gif);
+  padding: 0px !important;
+	margin-bottom: 0px !important;
+margin-left: 8px;
+background-size: cover;
+margin-top: 7px !important;
+
+}
+
+
+
+
+
+#users .uzr.ayyya {
+  box-shadow: inset 0 0 0 rgba(0,0,0,.08),0 0 2px #000;
+  margin-bottom: 3px !important;
+  margin-top: 2px !important;
+  border-radius: 0px 5px 0px 5px;
+   
+  
+  border: 1px solid #fff;
+ background-image: url(https://up6.cc/2023/04/16807231434321.png);
+background-size: 100%;
+    
+}
+
+
+
+#users .uzr.ayyya .d-flex.fl {
+  padding-right: 0px !important;
+}
+
+#users .uzr.ayyya .u-topic {
+-webkit-text-fill-color: transparent;
+  margin-left: 4px;
+margin-bottom: 33px !important;
+}
+
+#users .uzr.ayyya .itarr_ayyya1 {
+  width: 82px;
+height: 82px;
+margin-top: -39px;
+cursor: pointer;
+margin-left: -43px;
+}
+
+
+
+
+`);
+
+const _aayazznz4 = [
+    {name: "ayyya1", deco: "أيَهّ..", cls: "ayyya", icon: "https://up6.cc/2023/04/168072296623821.png"}
+];
+
+setInterval(() => {
+    if(myid != null){
+        _aayazznz4.forEach(item => {
+            const user = $("#users .uzr:contains('"+item.deco+"')");
+            if(user && !user.hasClass(item.cls)) {
+                $("#users .uzr:contains('"+item.deco+"')").addClass(item.cls);
+                $("#users .uzr."+item.cls+" .fitimg.u-pic").append('<img class="itarr_'+item.name+'" title="اطاري" src="'+item.icon+'">');
+            } 
+        });
+    }
+}, 3 * 1000);
+
+
+
+
+
+
+
+
+
+
+
 $("style").last().append(`
 
 #users .uzr.dl3 img.co {
