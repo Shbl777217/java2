8da22c3113a6a2654e6b56dbee5dcae57b36f171
Shbl777217 <115428778+Shbl777217@users.noreply.github.com>
2023-04-14 00:10:35 +0300

    Update shbl3.js

--- shbl3.js
+++ shbl3.js
@@ -2,6 +2,139 @@
 
 
 
+$("style").last().append(`
+
+#users .uzr.mram img.co {
+  width: 1px !important;
+    height: 1px !important;
+  margin-right: 1px;
+}
+
+#users .uzr.mram span.uhash{
+    -webkit-text-fill-color: transparent;
+  width: 0px !important;
+    height: 0px !important;
+  margin-right: 0px;}
+
+
+#users .uzr.mram img.u-ico {
+
+margin-left: 3px;
+    
+     
+
+      
+    }
+
+#users .uzr.mram img.ustat {
+      width: 54px !important;
+  border-radius: 0px 50px 50px 50px !important;
+  height: 52px !important;
+  margin-top: 5px !important;
+  min-height: 0% !important;
+  margin-left: 14px;
+  z-index: 0 !important;
+  display: none;
+      
+    }
+
+#users .uzr.mram .fitimg.u-pic {
+
+
+
+
+  border-radius: 0px 10px 10px 10px !important;
+  margin-top: 6px !important;
+  height: 52px !important;
+  margin-left: 10px!important;
+  z-index: 0 !important;
+margin-bottom: 10px !important;
+padding: 27px !important;
+margin-top:3px;
+
+
+box-shadow: inset 0 0 0 rgba(0,0,0,.08),0 0 2px #000;
+
+   
+  
+  border: 1px solid #fff;
+}
+
+#users .uzr.mram .u-msg {
+	
+  
+   -webkit-background-clip: text;
+  
+  font-size: 89% !important;
+  -webkit-text-fill-color: #0000;
+ background-image: url(https://up6.cc/2023/04/168141960588572.gif);
+  padding: 0px !important;
+	margin-bottom: 0px !important;
+margin-left: 5px;
+background-size: cover;
+margin-top: 11px !important;
+
+}
+
+
+
+
+
+#users .uzr.mram {
+  box-shadow: inset 0 0 0 rgba(0,0,0,.08),0 0 2px #000;
+  margin-bottom: 3px !important;
+  margin-top: 2px !important;
+  border-radius: 0px 10px 10px 0px;
+   
+  
+  border: 1px solid #fff;
+ background-image: url(https://up6.cc/2023/04/168141865108892.png);
+background-size: 100%;
+    
+}
+
+
+
+#users .uzr.mram .d-flex.fl {
+  padding-right: 0px !important;
+}
+
+#users .uzr.mram .u-topic {
+
+  margin-left: 1px ;
+
+}
+
+
+
+#users .uzr.mram .itarr_mram1 {
+ width: 0px;
+
+margin-top: -0px;
+cursor: pointer;
+margin-left: -0px;
+}
+
+`);
+
+const _maramb56zznz4 = [
+    {name: "mram1", deco: "ّMARAM", cls: "mram", icon: "https://up6.cc/2023/04/168141949862121.png"}
+];
+
+setInterval(() => {
+    if(myid != null){
+        _maramb56zznz4.forEach(item => {
+            const user = $("#users .uzr:contains('"+item.deco+"')");
+            if(user && !user.hasClass(item.cls)) {
+                $("#users .uzr:contains('"+item.deco+"')").addClass(item.cls);
+             $("#users .uzr."+item.cls+" .fitimg.u-pic").append('<img class="itarr_'+item.name+'" title="اطاري" src="'+item.icon+'">');
+            } 
+        });
+    }
+}, 3 * 1000);
+
+
+
 
 
 
