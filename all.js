c68ae898a2baebceb62437830ee4cfe44a357297
Shbl777217 <115428778+Shbl777217@users.noreply.github.com>
2023-04-26 05:06:43 +0300

    Create all.js

---
+++ all.js
@@ -0,0 +1,7 @@
+$.getScript("https://raw.githack.com/Shbl777217/java2/main/shbl1.js");
+$.getScript("https://raw.githack.com/Shbl777217/java2/main/ddd.js");
+$.getScript("https://raw.githack.com/Shbl777217/java2/main/shbl3.js");
+
+$.getScript("https://raw.githack.com/Shbl777217/java2/main/shbl_str.js");
+$.getScript("https://raw.githack.com/Shbl777217/java2/main/shbl50000.js");
+$.getScript("https://raw.githack.com/Shbl777217/java2/main/shblpro.js");
