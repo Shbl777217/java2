1fb3b04641eab9aa5496557c1daa50701b140df3
Shbl777217 <115428778+Shbl777217@users.noreply.github.com>
2023-05-02 08:42:23 +0300

    Update shbl3.js

--- shbl3.js
+++ shbl3.js
@@ -2679,112 +2679,6 @@ margin-left: -41px;
 
 
 
-$("style").last().append(`
-
-#users .uzr.fahd img.co {
-  width: 1px !important;
-    height: 1px !important;
-  margin-right: 1px;
-}
-
-#users .uzr.fahd span.uhash{
-    -webkit-text-fill-color: transparent;
-width: 1px !important;
-    height: 1px !important;
-  margin-right: 1px;}
-
-
-#users .uzr.fahd img.u-ico {
-margin-left: 9px;
-
-    border-radius: 20px 0px 20px 20px !important;
-     
-
-      
-    }
-
-#users .uzr.fahd img.ustat {
-      width: 54px !important;
-  border-radius: 0px 50px 50px 50px !important;
-  height: 52px !important;
-  margin-top: 5px !important;
-  min-height: 0% !important;
-  margin-left: 14px;
-  z-index: 0 !important;
-  display: none;
-      
-    }
-
-#users .uzr.fahd .fitimg.u-pic {
-  border-radius: 100px !important;
-  margin-top: 12px !important;
-  height: 52px !important;
-  margin-left: 6px!important;
-  z-index: 0 !important;
-margin-bottom: 11px !important;
-padding: 29px !important;
-border:1px solid #000;margin-top:3px;border-radius:100px;
-
-box-shadow:inset 0 0 0 rgba(0,0,0,.08),0 0 6px #171717
-}
-
-#users .uzr.fahd .u-msg {
--webkit-background-clip: text;
-  
-  font-size: 93% !important;
-  -webkit-text-fill-color: #0000;
- background-image: url(https://up6.cc/2023/03/167832696716741.gif);
-  padding: 0px !important;
-	margin-bottom: 20px !important;
-margin-right: 28px !important;
-margin-left: 15px;
-background-size: cover;
-margin-top: 10px !important;
-
-}
-
-
-
-
-
-#users .uzr.fahd {
-  box-shadow: inset 0 0 0 rgba(0,0,0,.08),0 0 2px #000;
-  margin-bottom: 3px !important;
-  margin-top: 2px !important;
-  border-radius: 0px 18px 18px 18px;
-   
-  
-  border: 1px solid #000;
- background-image: url(https://up6.cc/2023/03/167832839457791.png);
-background-size: 100%;
-    
-}
-
-
-
-#users .uzr.fahd .d-flex.fl {
-  padding-right: 0px !important;
-}
-
-#users .uzr.fahd .u-topic {
- -webkit-text-fill-color: transparent;
-  margin-left: 9px ;
-
-}
-
-
-
-#users .uzr.fahd .itarr_fahd {
-height: 0px;
-margin-top: -40px;
-cursor: pointer;
-margin-left: -41px;
-}
-
-`);
-
-
-    
 
 
 
@@ -4487,9 +4381,6 @@ const _ma56zznz2 = [
 
 {name: "awham", deco: "𝐀𝐖𝐇𝐀𝐌", cls: "awham", icon: "https://up6.cc/2023/04/168193932148781.png"} ,
 
-
-
-{name: "fahd", deco: "𝐅𝐀𝐇𝐀𝐃 ⅏", cls: "fahd", icon: "https://up6.cc/2023/04/168193932148781.png"} ,
 
 {name: "sauudg", deco: "سعُـود..", cls: "sauudg", icon: "https://up6.cc/2023/04/168193932148781.png"} ,
 
