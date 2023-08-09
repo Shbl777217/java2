24fc45118a01ffdedcce2b12a69411321530c4d5
Shbl777217 <115428778+Shbl777217@users.noreply.github.com>
2023-08-10 02:39:44 +0300

    Update shbl3.js

--- shbl3.js
+++ shbl3.js
@@ -188,7 +188,7 @@ box-shadow: inset 0 0 0 rgba(0,0,0,.08),0 0 1px #000;
   margin-top: 2px !important;
   border-radius: 4px 4px 4px 4px;
    
-  background-image: url(https://up6.cc/2023/08/169152920829051.jpg);
+  background-image: url(https://up6.cc/2023/08/169162435722121.jpg);
 background-size: cover;
   border: 1px solid #fff;
 
