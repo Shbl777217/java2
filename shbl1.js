175d5fc7577ea1b59010738b0e4ba6d4562d7d23
Shbl777217 <115428778+Shbl777217@users.noreply.github.com>
2023-09-04 00:51:56 +0300

    Update shbl1.js

--- shbl1.js
+++ shbl1.js
@@ -6,6 +6,9 @@
 
 
 
+
+
+
 $(`<style>
 .nosel.d-flex.room.r18700b42ce5x137168c7-x-ejgokl .fitimg.u-pic.borderg {
   letter-spacing: 1px;
@@ -260,13 +263,11 @@ border-radius:  8px 0 8px!important;
 #wall.roh .fitimg.u-pic.borderg {
 height: 52px !important;
 border: 1px solid;
-border-radius: 100px ;
-box-shadow: 0 0 0px rgb(0, 0, 0), inset 0 0 4px rgb(0, 0, 0), 0 0 0 0px #707070;
+border-radius:  8px 0 8px;
+box-shadow: 0 0 0px rgb(0 0 0), inset 0 0 4px rgb(0 0 0), 0 0 0 0px #000;
 margin-top: 2px;
 margin-right: 2px;
 }
-
-
 #rooms.roh .fitimg.u-pic.borderg{
 height: 52px !important;
 border: 1px solid;
@@ -616,7 +617,15 @@ background-color: #000 !important;
 
     $("style").last().append(`
 
-
+	#wall.roh .fitimg.u-pic.borderg {
+height: 52px !important;
+border: 1px solid;
+border-radius: 100px ;
+box-shadow: 0 0 0px rgb(0, 0, 0), inset 0 0 4px rgb(0, 0, 0), 0 0 0 0px #707070;
+margin-top: 2px;
+margin-right: 2px;
+}
+  
     
     #upro.melaaco .border {
   border-radius: 10px 10px  10px 10px !important;
