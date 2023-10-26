0a6439d49bb8c07c6bef5aa36837ac17a3d937af
Shbl777217 <115428778+Shbl777217@users.noreply.github.com>
2023-10-26 05:40:57 +0300

    Update shbl3.js

--- shbl3.js
+++ shbl3.js
@@ -72,7 +72,7 @@ padding: 26px !important;
  background-image: url(https://up6.cc/2023/03/167832696716741.gif);
   padding: 0px !important;
 	margin-bottom: 3px !important;
-margin-right: 28px !important;
+margin-right: 8px !important;
 margin-left: 5px;
 background-size: cover;
 margin-top: 2px !important;
