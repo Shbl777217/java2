c85be6658dc644393b04867bb6b61dd3d6227fbb
Shbl777217 <115428778+Shbl777217@users.noreply.github.com>
2023-12-23 22:19:36 +0300

    Update 3rooth.js

--- 3rooth.js
+++ 3rooth.js
@@ -1,5 +1,53 @@
 
 
 
-$(`<center><div style="position: static;width: 100%;" onclick="setTimeout(function(){fixSize();},800);"><div width="99.5%" id="design-a"style="background-color: #ffe0b6;color: #105873;border-bottom: 3px solid #105873;border-top: 3px solid #6b5f5f;padding-bottom: 2px;border-radius: 0px 0px 15px 15px;" ><font id="sultan" class=" sultan11"><u><b>الاشتراكــات</b></u> </font><div id="spanan" style="font-family: jazeera-light, FontAwesome;color: red;text-align: right;display:none;" ><p style="text-align: center"><b><font color="#000080">اشتراكات الشات</font></b></p></p><div style="float: right;padding-right: 20px;">- 1</div> تاج ذهبي بخاصية ( المخفي ) + ( سوبر فضي هديه ) بـ 600 ريال لمدة : شهرين <img src="https://www.te3b.com/sico/z1d1hdjuci10.gif"></p><div style="float: right;padding-right: 20px;">- 2</div> تاج اسود ملكي بخاصية ( المخفي ) بـ 500 ريال لمدة : شهرين <img src="https://www.te3b.com/sico/z1d3c130e710.GIF"></p><div style="float: right;padding-right: 20px;">- 3</div> تاج فضي بـ 400 ريال لمدة : شهرين <img src="https://www.te3b.com/sico/z1d3c137ha10.gif"> </p><div style="float: right;padding-right: 20px;">- 4</div> نجمه ذهبيه بـ 300 ريال لمدة : شهرين <img src="https://www.te3b.com/s1.gif"></p><div style="float: right;padding-right: 20px;">- 5</div> نجمه سوداء بـ 150 ريال لمدة : شهر <img src="https://www.te3b.com/s2.gif"></p><div style="float: right;padding-right: 20px;">- 6</div> نجمه فضيه بـ 100 ريال لمدة : شهر <img src="https://www.te3b.com/s3.gif"><p><b>ومن اراد بنر لـ اسمه بـ 100 ريال مع التصميم ولمدة شهر</font></b><p style="text-align: center"><u><b><a target="_blank" href="https://www.te3b.com/sico/z1f5c4o6s610.JPG">وللتواصل مع الاداره اضغط هنا</a></b></u></font></div> </div></div><center>`).insertBefore('#d2');$("#design-a").click(function(){$("#spanan").slideToggle(function(){});});
 
+
+$(`<center><div style="position: static;width: 100%;" onclick="setTimeout(function(){fixSize();},800);"><div width="99.5%" id="design-a"style="background-color: antiquewhite;color: #105873;border-bottom: 3px solid #105873;border-top: 3px solid #6b5f5f;padding-bottom: 2px;border-radius: 0px 0px 15px 15px;" >
+	<font id="sultan0" class=" sultan11"style="
+    font-family: jazeera-light, FontAwesome;
+" >  <font color="#FF0000">  <u><b>بمناسبة رأس السنه / اضغط هنا</b></u></font></font><font id="sultan" class=" sultan11"><u><b> </b></u></font><div id="spanan"style="font-family: jazeera-light, FontAwesome;color: red;text-align: right;display:none;"><p style="text-align: center">
+			<b><font color="#000080"><font size="2">عروض مخفضه للمشتركين / بمناسبة 
+		قرب رأس السنه الميلاديه<span lang="ar-ye">
+			</span>&nbsp;2024</font><span lang="ar-ye">
+			</span>&nbsp;</font></b></p></p><div style="float: right;padding-right: 20px;">-1</div>  
+		<img src="https://www.alriad.net/s2.gif"><img src="https://alriad.net/sico/znd9wo95R.gif"><b><font size="2"><img src="https://alriad.net/sico/z16ed380f0bfe6e4.gif"> 
+		اشترك ادمن ذهبي </font><font size="2" color="#0000FF">شهرين </font>
+		<font face="Ebrima" size="2" color="#000000">1400</font><font size="2"> 
+		واحصل ع</font><font color="#008000" size="2"> </font></b>
+		<font color="#000000" size="2"><b>(ادمن اسود 
+		وتاج ذهبي ونجمه)</b></font><b><font size="2"><font color="#000000">ـ</font>
+		<font color="#008080"> <span lang="ar-ye"> &nbsp;</span>&nbsp;</font><img src="https://www.alriad.net/sico/z1c1ib2ng710.gif"></font></b></p>
+		<div style="float: right;padding-right: 20px; width:34px; height:19px">
+			2</div><font size="2"><b> <img src="https://www.alriad.net/sico/z1d3c130e710.GIF"><img src="https://www.alriad.net/sico/z1d3c130e710.GIF"> اشترك ادمن اسود
+		<font color="#0000FF">شهرين </font><font face="Ebrima" color="#000000">1150</font> 
+		واحصل ع<font color="#008000"> </font></b><font color="#000000"><b>(تاجين 
+		اسود)</b></font></font><b><font size="2" color="#000000">ـ</font><font size="2">
+		<font color="#008080"> <span lang="ar-ye"> &nbsp;</span>&nbsp;</font><img src="https://alriad.net/sico/z16ed380f0bfe6e4.gif"></font></b></p><div style="float: right;padding-right: 20px;">-3</div>
+		<font size="2"><b><img src="https://www.alriad.net/s1.gif"><img src="https://www.alriad.net/s1.gif"> 
+		اشترك تاج ذهبي <font color="#008080"><u> مخفي </u></font>
+		<font color="#0000FF"> شهرين<span lang="ar-ye">
+			</span> </font><font face="Ebrima"><font color="#000000">500</font> </font></b>
+		<span lang="ar-ye"><b>واحصل ع <font color="#000000">(نجمتين ذهبيه)</font></b></span></font><b><font size="2" color="#000000">ـ</font><font size="2">  
+		</font></b><img src="https://alriad.net/sico/znd9wo95R.gif"></p><div style="float: right;padding-right: 20px;">-4</div><img src="https://www.alriad.net/s2.gif"><img src="https://www.alriad.net/s2.gif"> 
+		<font size="2"><b>اشترك تاج اسود <u><font color="#008080"> مخفي </font></u>
+		<font color="#0000FF"> &nbsp;شهرين<span lang="ar-ye">
+			</span> </font><font face="Ebrima" color="#000000">400</font><font color="#0000FF"> </font>
+		واحصل ع </b> <font color="#000000"><b>(نجمتين سودا)</b></font></font><b><font size="2" color="#000000">ـ</font></b> <img src="https://www.alriad.net/sico/z1d3c130e710.GIF"></p><div style="float: right;padding-right: 20px;">-5</div>
+		<font size="2"><b><img src="https://www.alriad.net/s3.gif"><img src="https://www.alriad.net/s3.gif"> 
+		اشترك تاج فضي&nbsp; <font color="#0000FF"> شهرين </font>
+		<font face="Ebrima" color="#000000">300</font><font color="#0000FF"> </font>واحصل ع
+		<font color="#000000">(نجمتين فضيه)ـ</font> &nbsp;<img src="https://www.alriad.net/sico/z1d3c137ha10.gif"></b></font></p><div style="float: right;padding-right: 20px;">
+			-6</div><font size="2"><b>&nbsp;اشترك ( عشره نجوم ذهبيه )
+		<font color="#0000FF">شهر</font> <font face="Ebrima" color="#000000">1100</font> 
+		واحصل ع <font color="#000000">(روم خاص)ـ</font> <img src="https://www.alriad.net/s1.gif"><img src="https://www.alriad.net/s1.gif"></b></font></p><div style="float: right;padding-right: 20px;">
+			-7</div><font size="2">&nbsp;<img src="https://www.alriad.net/s1.gif"> 
+		<b>اشترك</b> <b>( عشره نجوم سودا )</b> <b>
+		<font color="#0000FF">شهر</font></b> <b>
+		<font face="Ebrima" color="#000000">900</font></b> 
+		<b>واحصل ع</b> <b><font color="#000000">( نجمه ذهبيه )</font></b><font color="#000000">ـ</font> <img src="https://www.alriad.net/s2.gif"><img src="https://www.alriad.net/s2.gif"></font><b></p>
+		<p style="text-align: center"><u><font color="#0000FF" size="2">ملاحظه : ينتهي العرض 
+		اول ايام راس السنه / وكل عام وانتم بخير</font></u></font><p style="text-align: center"><u>
+		<a target="_blank" href="https://alriad.net/sico/z1f5c4o6s610.JPG">
+		<font size="2">و للتواصل مع الاداره اضغط هنا</font>
+		</a></u></font></div></div></div><center>`).insertBefore('#d2');$("#design-a").click(function(){$("#spanan").slideToggle(function(){})});
