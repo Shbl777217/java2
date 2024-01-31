9b99d7fbe925523dc2d0a7b78360e8fd4a991c8f
Shbl777217 <115428778+Shbl777217@users.noreply.github.com>
2024-01-31 03:19:12 +0300

    Update shbl3.js

--- shbl3.js
+++ shbl3.js
@@ -73,7 +73,7 @@ border:1px solid #fbfcfc;margin-top:1px;
 
 box-shadow:inset 0 0 0 rgba(0,0,0,.08),0 0 3px #000
 filter: hue-rotate(360deg);
-box-shadow: 0 0 3px rgb(0 0 0), inset 0 0 4px rgb(0 0 0), 0 0 0 0px #000;
+box-shadow: 0 0 3px rgb(0 0 0), inset 0 0 0px rgb(0 0 0), 0 0 0 0px #000;
 }
 
 #users .uzr.tro7rm .u-msg {
