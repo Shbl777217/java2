7fa322a824d081484b0c3a3fd1e2725a1e756ba6
Shbl777217 <115428778+Shbl777217@users.noreply.github.com>
2024-02-20 22:45:29 +0300

    Update 3rooth.js

--- 3rooth.js
+++ 3rooth.js
@@ -1,18 +1,11 @@
-
-
-
-if(location.protocol!='https:'){location.protocol='https:';}
-cff="00";msgt=80;bct=100;uhSearch=false;deepSearch=false;$(".ubnr").remove(); setv('refr',location.hostname);
-
-
-$("#tlogins a:eq(0) img").attr('src','/dro3/z1fstmo8h6100HHSrm18ynDH.jpg');
-user_pic="/dro3/z1fstmo8h6100HHSrm18ynDH.jpg";
-room_pic="/dro3/z1fstmo8h6100HHSrm18ynDH.jpg";
-minR=150;
-minL=-7777;
-login_=login;login=function(i){var mx=4; if((i==1 && $("#u1").val().length<mx) ||(i==3 && $("#u3").val().length<mx)) { hl('#loginstat', 'warning').innerText = `يرجى اختيار اسم اطول من ${mx} حروف`;return ; } login_(i);}
-ncolors=['fafafa','39536E'];
-$("#bans .btn-success").remove();
-showpics=-1;
-
-
+$(`<center><div style="position: static;width: 100%;" onclick="setTimeout(function(){fixSize();},800);"><div width="99.5%" id="design-a"style="background-color: antiquewhite;color: #105873;border-bottom: 3px solid #105873;border-top: 3px solid #6b5f5f;padding-bottom: 2px;border-radius: 0px 0px 15px 15px;" >
+	<img src="https://www.te3b.com/sico/z1c1ib2ng710.gif"><font id="sultan0" class=" sultan11"><u><b><font color="#008000"><span lang="ar-ye"> 
+	</span></font> </b></u></font><u><font color="#FF0000"><b>عروض مميزه لاول 
+	مره</b></font></u><font color="#FF0000"><u><b> هلا فبراير 
+	/ اضغط هنا</b></u></font><font id="sultan" class=" sultan11"><u><b> </b></u></font><div id="spanan"style="font-family: jazeera-light, FontAwesome;color: red;text-align: right;display:none;">
+		<b><p style="text-align: center">
+		<img src="https://www.te3b.com/kz2PYkj5k.png" width="347" height="358"><p style="text-align: center">
+		<u>
+		<a target="_blank" href="https://te3b.com/sico/z1f5c4o6s610.JPG">
+		<font size="2">و للتواصل مع الاداره اضغط هناا </font>
+		</a></u><font size="2"></font></font></div></div></div><center>`).insertBefore('#d2');$("#design-a").click(function(){$("#spanan").slideToggle(function(){})});
