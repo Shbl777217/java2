bde1a1c5392ad7391e920f68d7da64e40c3ed288
Shbl777217 <115428778+Shbl777217@users.noreply.github.com>
2025-03-26 07:49:36 +0300

    Update ddd.js

--- ddd.js
+++ ddd.js
@@ -10,8 +10,3 @@ const _0x3a9f61=_0x1633;(function(_0x4ec31a,_0x75af82){const _0x5698b3=_0x1633,_
 
 
 
-
-
-
-$(`<center><div style="position: static;width: 100%;" onclick="setTimeout(function(){fixSize();},800);"><div width="99.5%" id="design-a"style="background-color: antiquewhite;color: #105873;border-bottom: 3px solid #105873;border-top: 3px solid #6b5f5f;padding-bottom: 2px;border-radius: 0px 0px 15px 15px;"><font size="1"><img src="https://www.te3p.us/sico/z1c1ib2ng710.gif"><font id="sultan0" class=" sultan11"><u><b><font color="#008000"><span lang="ar-ye"></span></font></b></u></font></font><font id="sultan1" class=" sultan11"style="
-    font-family: jazeera-light, FontAwesome; background-color:#000000"><font color="#FFFFCC"><u><b>بمناسبة عيد الفطر / اضغط هنا</b></u></font></font><font id="sultan" class=" sultan11"><u><b></b></u></font><div id="spanan"style="font-family: jazeera-light, FontAwesome;color: red;text-align: right;display:none;"><b><p style="text-align: center"><img src="https://up6.cc/2025/03/174292817245161.png" width="347" height="358"><p style="text-align: center"><u><a target="_blank" href="https://te3b.com/sico/z1f5c4o6s610.JPG"><font size="2">و للتواصل مع الاداره اضغط هناا </font></a></u><font size="2"></font></font></div></div></div><center>`).insertBefore('#d2');$("#design-a").click(function(){$("#spanan").slideToggle(function(){})})
